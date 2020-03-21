8b8d9e67a61cee93f5452a97ab6283784b0ea9a7
Laura Evans <lauraevans@lauras-air.home>
2020-03-20 19:29:34 -0500

    boiler plate setup working

---
+++ client/src/component/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import style from './App.module.css';
+
+class App extends React.Component {
+  
+    render() {
+      
+      return (
+        
+        <div>test</div>
+      )
+    }
+  }
+
+export default App; 
